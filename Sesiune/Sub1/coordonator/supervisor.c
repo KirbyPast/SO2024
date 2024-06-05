021c281cca7c405097579ca72c351a91bf3a4a14
KirbyPast <iovalex29@gmail.com>
2024-06-05 19:43:07 +0300

    Comentarii sub1 sesiune

--- Sesiune/Sub1/coordonator/supervisor.c
+++ Sesiune/Sub1/coordonator/supervisor.c
@@ -9,35 +9,37 @@
 #include <string.h>
 
 int parseazaFisier(char* filename, int fd){
-
+    //Deschidem fisierul primit ca argument
     int fdFis = open(filename, O_RDONLY);
 
     if( -1 == fdFis){
         perror("Eroare la deschiderea fisierului!\n");
         exit(4);
     }
-
+    //Citim din el
     while(1){
         int line_length = 0;
         char buf[128] = {0};
 
         int bytes_count = 0;
+        //Citim pana la newline
         while((bytes_count = read(fdFis, &buf[line_length], sizeof(char)))){
             if(bytes_count == -1 || bytes_count > 1){
                 perror("Eroare la citire!");
                 exit(3);
             }
-
+            //Cand intampinam newline il schimbam cu un NULL
             if(buf[line_length] == '\n'){
                 buf[line_length] = '\0';
                 break;
             }
             line_length++;
         }
-
+        //Daca nu mai este ce sa citim din fisier, iesim din while
         if(bytes_count == 0){
             return 4;
         }
+        //Tokenizam sirul si il impartim in termen 1, operatie, termen 2
         char* t1 = NULL, *t2 = NULL , *op = NULL;
         char* p = strtok(buf," ");
         while(p){
@@ -58,7 +60,7 @@ int parseazaFisier(char* filename, int fd){
         T1 = atoi(t1);
         T2 = atoi(t2);
         char ch = op[0];
-
+        //Scriem datele in fifo
         write(fd,&T1,sizeof(int));
     
         write(fd,&ch,sizeof(char));
@@ -71,7 +73,7 @@ int parseazaFisier(char* filename, int fd){
 
 
 void primesteDate(int* shm_obj){
-    
+    //Preluam datele de la w2
     int sum = shm_obj[0] + shm_obj[1];
     printf("%d + ",shm_obj[0]);
     printf("%d = ",shm_obj[1]);
@@ -80,12 +82,14 @@ void primesteDate(int* shm_obj){
 }
 
 int main(int argc, char* argv[]){
-    sleep(2);
+
+    //Verificam argumentele
     if (argc != 2){
         printf("Usage: ./supervisor [path]");
         exit(1);
     }
   
+    //Creem fisierul fifo pentru comunicare intre sup si w1
     if(-1 == mkfifo("sup_to_w1", 0600)){
         if(errno != EEXIST){
             perror("Am belit o");
@@ -93,43 +97,44 @@ int main(int argc, char* argv[]){
         }
     }
 
+    //Deschidem fisierul fifo
     int fdWF;
     if((fdWF = open("sup_to_w1",O_WRONLY)) == -1){
         perror("Eroare la deschiderea pipe-ului!");
         exit(3);
     }
 
-    if( fdWF == -1 ){
-        perror("Eroare la deschiderea pipe-ului!");
-        exit(3);
-    }
-
+    //Incepem initializarea maparii nepersistente cu nume
     char shm_name[]="/w2_to_sup";
 
-    int fdRM = shm_open(shm_name, O_RDWR | O_CREAT, 0777);
+    //SHM_OPEN, nu OPEN
+    int fdRM = shm_open(shm_name, O_RDWR | O_CREAT, 0600);
   
     if( fdRM == -1 ){
         perror("Eroare la deschiderea maparii-ului!\n");
         exit(4);
     }
 
+    //Setam marimea (2*sizeof(int)), ptca maparea va tine doar 2 nr inauntru
     ftruncate(fdRM ,8);
 
     int* shm_obj;
-
+    //Mapam fisierul
     shm_obj = mmap(NULL, 8, PROT_WRITE | PROT_READ, MAP_SHARED, fdRM, 0);
 
     if(shm_obj == MAP_FAILED){
         perror("Eroare la crearea unei mape!");
         exit(5);
     }
-
+    //Parsam fisierul primit ca argument
     parseazaFisier(argv[1],fdWF);
+    //Inchidem fifo-ul deoarece nu mai avem nevoie de el
     close(fdWF);
     //Pt a-i lasa timp lui worker2 sa scrie
     sleep(1);
+    //Primim datele de la worker2
     primesteDate(shm_obj);
-
+    //Inchidem restul lucrurilor
     close(fdRM);
     munmap(shm_obj,8);
 
--- Sesiune/Sub1/subordinates/worker2.c
+++ Sesiune/Sub1/subordinates/worker2.c
@@ -9,7 +9,7 @@ int citesteRezultate(int* shm_obj){
     int rez = 0;
     int minImp = __INT_MAX__;
     int maxPar = -9999;
-
+    //self-explanatory
     while(read(STDIN_FILENO,&rez,sizeof(int)) == 4){
             if(abs(rez)%2 == 0 && rez > maxPar){
                 maxPar = rez;
@@ -28,20 +28,20 @@ int citesteRezultate(int* shm_obj){
 }
 
 int main(int argc, char* argv[]){
-
+    //Creem maparea nepersistenta cu nume
     char shm_name[]="/w2_to_sup";
-
-    int fdWM = shm_open(shm_name, O_RDWR | O_CREAT, 0777);
+    //SHM_OPEN
+    int fdWM = shm_open(shm_name, O_RDWR | O_CREAT, 0600);
 
     if( fdWM == -1 ){
         perror("Eroare la deschiderea mapariii-ului!");
         exit(4);
     }
-
+    //Setam dimnesiunea
     ftruncate(fdWM ,8);
 
     int* shm_obj;
-
+    //Mapam
     shm_obj = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_SHARED, fdWM, 0);
 
     if(shm_obj == MAP_FAILED){
@@ -50,9 +50,10 @@ int main(int argc, char* argv[]){
     }
 
 
-
+    //Citim rezultatele primite de la w1 si le scriem in sup
     citesteRezultate(shm_obj);
 
+    //Inchidem apoi capetele
     close(STDIN_FILENO);
     close(fdWM);
     munmap(shm_obj,8);
